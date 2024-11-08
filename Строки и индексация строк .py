cb8d53028346acfd3772c3ddb49e8ff0e54815a7
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-11-08 03:11:31 +0200

    Add files via upload

---
+++ Строки и индексация строк .py
@@ -0,0 +1,7 @@
+# Практическое задание по уроку "Строки и индексация строк":
+example = 'Привет'
+print(example[0])
+print(example[-1])
+print(example[(len(example) // 2) :])
+print(example[::-1])
+print(example[::2]) # почему первые , а не втроные буквы????
